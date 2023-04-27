91d13dbb7a1e66005a3b91831a69fc425f21c5f3
malek <malzovich@gmail.com>
2023-04-27 16:57:09 +0100

    Layout components

---
+++ components/layout/SidebarItem.tsx
@@ -0,0 +1,34 @@
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+  href?: string;
+  label: string;
+  icon: IconType;
+  onClick?: () => void;
+}
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  href,
+  label,
+  icon: Icon, // Alias Typescript
+  onClick,
+}) => {
+  return (
+    <div className="flex items-center">
+      <div
+        className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 
+           hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden"
+      >
+        <Icon size={28} color="white" />
+      </div>
+      <div
+        className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10
+      cursor-pointer "
+      >
+        <Icon size={24} color="white" />
+        <p className="hidden lg:block text-white text-xl">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarItem;
